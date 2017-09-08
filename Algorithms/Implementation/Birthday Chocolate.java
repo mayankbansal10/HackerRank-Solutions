423f8cfd498c3a55a7aeac04009752514e482022
Mayank Bansal <mayankbansal10@gmail.com>
2017-09-08 21:42:16 +0530

    Create Birthday Chocolate.java

---
+++ Algorithms/Implementation/Birthday Chocolate.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int getWays(int[] squares, int d, int m){
+        // Complete this function
+        int n=0,sum=0;
+			 for(int i=0;i<squares.length-m+1;i++)
+			 {
+				sum=0;
+				for(int j=i;j<m+i;j++)
+				{
+					sum=sum+squares[j];
+		        }
+				if(d==sum)
+					n++;
+			 }
+			 return n;
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        int d = in.nextInt();
+        int m = in.nextInt();
+        int result = getWays(s, d, m);
+        System.out.println(result);
+    }
+}
